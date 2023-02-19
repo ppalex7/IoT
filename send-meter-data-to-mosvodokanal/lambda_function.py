03faaebfc639e774e66a2a9c69121220423a3ce2
Alexandr Duplishchev <ppalex@yandex-team.ru>
2023-02-19 17:17:12 +0400

    разбить длинные запросы

--- send-meter-data-to-mosvodokanal/lambda_function.py
+++ send-meter-data-to-mosvodokanal/lambda_function.py
@@ -53,18 +53,37 @@ def submit_meters(cold, hot):
         raise Exception('Authorization failed: ' + r_auth.text)
     ch = {'cookies': r_auth.cookies, 'headers': {'Referer': api_url}}
 
-    r_account = requests.get(report_url, params={'_dc': dc(), 'reportId': 'NATURAL_PERSON_USER_ACCOUNT_REGISTER', 'page': 1, 'start': 0, 'limit': 25}, **ch)
+    r_account = requests.get(
+        report_url,
+        params={
+            '_dc': dc(),
+            'reportId': 'NATURAL_PERSON_USER_ACCOUNT_REGISTER',
+            'page': 1,
+            'start': 0,
+            'limit': 25,
+            },
+        **ch)
     logger.debug("Got NATURAL_PERSON_USER_ACCOUNT_REGISTER report: %d, %s", r_account.status_code, r_account.content)
     r_account.raise_for_status()
     account_id = r_account.json()['list'][0]['ID']
 
-    r_readreq = requests.post(api_url + 'api/NewMeterReadRequest/getOrCreateMeterReadRequest', data={'accountId': account_id}, **ch)
+    r_readreq = requests.post(
+        api_url + 'api/NewMeterReadRequest/getOrCreateMeterReadRequest',
+        data={'accountId': account_id},
+        **ch)
     logger.debug("MeterReadRequest response: %d, %s", r_readreq.status_code, r_readreq.content)
     r_readreq.raise_for_status()
     request_id = r_readreq.json()['requestId']
     param_list = [{'type': 'REQUEST_ID', 'value': request_id}]
 
-    r_meters = requests.post(report_url, params={'_dc': dc()}, data={'reportId': 'NATURAL_PERSON_METER_READ_REQUEST_PART_DATA_REESTR', 'parameterList': json.dumps(param_list)}, **ch)
+    r_meters = requests.post(
+        report_url,
+        params={'_dc': dc()},
+        data={
+            'reportId': 'NATURAL_PERSON_METER_READ_REQUEST_PART_DATA_REESTR',
+            'parameterList': json.dumps(param_list),
+        },
+        **ch)
     logger.debug("Got NATURAL_PERSON_METER_READ_REQUEST_PART_DATA_REESTR response: %d, %s", r_meters.status_code, r_meters.content)
     r_meters.raise_for_status()
     meters = r_meters.json()['list']
@@ -82,7 +101,10 @@ def submit_meters(cold, hot):
         ]
     }
 
-    submit = requests.post(api_url + 'api/NaturalPersonMeterRead/send', json=req_data, **ch)
+    submit = requests.post(
+        api_url + 'api/NaturalPersonMeterRead/send',
+        json=req_data,
+        **ch)
     logger.debug("Meters submited: %d, %s", submit.status_code, submit.content)
     submit.raise_for_status()
     return submit.json()
